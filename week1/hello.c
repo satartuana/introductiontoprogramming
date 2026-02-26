179691ff6cbdbb4f8dfda81e2bb702884ddc2453
satartuana <satar.stuana@ibu.edu.mk>
2026-02-26 22:03:55 +0300

    Update hello.c

--- week1/hello.c
+++ week1/hello.c
@@ -18,9 +18,10 @@
 
 int main(void)
 {
-    // TODO: Prompt the user for their name using get_string()
+  string name = get_string("What is your name? ");
+printf("Hello, %s\n", name); 
 
 
-    // TODO: Print "hello, " followed by their name using printf()
+  
 
 }
